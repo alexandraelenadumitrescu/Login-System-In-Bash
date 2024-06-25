15f79c33ff9eb7a6994766364e830782dc60dce6
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2024-06-25 20:26:40 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while IFS=',' read -r ID nume prenume email parola tara
+  do
+    echo $ID
+    echo $nume
+    echo $email
+    echo $parola
+    echo $tara
+    echo $last_login
+    echo -------------------
+  done < <(tail -n +2 fisier.csv)
+
+
+
+
+
+
